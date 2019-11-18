fa9579e6017365ddb8afa16e93254aa7fd423ae0
Ludymaps <41130400+Mikeyudex@users.noreply.github.com>
2019-11-18 17:47:56 -0500

    Mapa de Finca los Empalmes, se agrega leyenda y nuevas capas

---
+++ data/Cable_Via_4.js
@@ -0,0 +1,21 @@
+var json_Cable_Via_4 = {
+"type": "FeatureCollection",
+"name": "Cable_Via_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NOMBRE": "CABLE VIA #10" }, "geometry": { "type": "LineString", "coordinates": [ [ -74.159667084930319, 10.905269485641805 ], [ -74.158626243151872, 10.904738455652925 ] ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "CABLE VIA #6" }, "geometry": { "type": "LineString", "coordinates": [ [ -74.158030132125447, 10.907042042941766 ], [ -74.158350800840736, 10.908940449773345 ] ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "CABLE VIA #5" }, "geometry": { "type": "LineString", "coordinates": [ [ -74.158441795266754, 10.907168500566691 ], [ -74.157279994927904, 10.906825268717837 ] ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "CABLE VIA #4" }, "geometry": { "type": "LineString", "coordinates": [ [ -74.156968492205309, 10.904963019812444 ], [ -74.157644249708909, 10.909002853193609 ] ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "CABLE VIA #3" }, "geometry": { "type": "LineString", "coordinates": [ [ -74.156731756911171, 10.909092300042261 ], [ -74.156072140400383, 10.905261585341375 ] ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "CABLE VIA #1" }, "geometry": { "type": "LineString", "coordinates": [ [ -74.155917733767296, 10.909447346534712 ], [ -74.15532364175327, 10.906190123044206 ] ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "CABLE VIA #7" }, "geometry": { "type": "LineString", "coordinates": [ [ -74.155642268960776, 10.905451544423325 ], [ -74.159319094666486, 10.904085478943871 ] ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "CABLE VIA #13" }, "geometry": { "type": "LineString", "coordinates": [ [ -74.159712265561296, 10.903504525667774 ], [ -74.159464979928501, 10.902367756893739 ] ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "LineString", "coordinates": [ [ -74.159471132439521, 10.902396040015319 ], [ -74.158856303269175, 10.902744718347154 ] ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "CABLE VIA #8" }, "geometry": { "type": "LineString", "coordinates": [ [ -74.157974750001529, 10.905188768147321 ], [ -74.157535000337646, 10.902639480011395 ] ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "CABLE VIA #9" }, "geometry": { "type": "LineString", "coordinates": [ [ -74.158193677318977, 10.902937641770423 ], [ -74.158855645250043, 10.905396480073236 ] ] } },
+{ "type": "Feature", "properties": { "NOMBRE": "CABLE VIA #12" }, "geometry": { "type": "LineString", "coordinates": [ [ -74.158555423143682, 10.901784165640272 ], [ -74.159401402321208, 10.904022173903458 ], [ -74.159319094666486, 10.904085478943871 ] ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "LineString", "coordinates": [ [ -74.154323368290122, 10.90653193968356 ], [ -74.15624975004954, 10.905945575317096 ] ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "LineString", "coordinates": [ [ -74.155053398944631, 10.90813878551316 ], [ -74.154725015610936, 10.906336072731216 ] ] } }
+]
+}
